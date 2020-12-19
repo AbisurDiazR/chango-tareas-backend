da8fc55e2cb732b2e92c899db4b7dfcc1b263271
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-12-18 18:37:11 -0600

    Last updates about mercadopago

--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,17 @@ const userSchema = new Schema({
     password: String,
     nivel: String,
     rol: String,
-    materias: String 
+    materias: String,
+    credenciales: {
+        access_token: String,
+        expires_in: Number,
+        live_mode: Boolean,
+        public_key: String,
+        refresh_token: String,
+        scope: String,
+        token_type: String,
+        user_id: Number
+    }
 }, {
     timestamps:true //automaticamene agrega el campo created at y updated at
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,102 +8,177 @@ const Tarea = require('../models/tareas');
 
 const Mensaje = require('../models/mensaje');
 
+// SDK de Mercado Pago
+const mercadopago = require('mercadopago');
+
 //importamos el jsonwebtoken
 const jwt = require('jsonwebtoken');
 var ObjectId = require('mongodb').ObjectID;
 
-router.get('/', (req,res)=>{
+//variable fetch
+var fetch = require('node-fetch');
+
+//Agregamos credenciales
+mercadopago.configure({
+    access_token: 'TEST-5324311744444569-112619-2cacd777bcc9b20a253ec3bb3204e92d-7908615'
+});
+
+router.get('/', (req, res) => {
     res.send('Hello World');
     console.log(req.body)
 });
 
+//ruta para mercado pago credenciales
+router.post('/credenciales', (req, res) => {
+    posData('https://api.mercadopago.com/oauth/token', req.body).then(data => {
+        //console.log(data);
+        res.send({ data: data });
+    });
+});
+
+//petición curl a mercadopago usando fetch
+async function posData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+}
+
+//ruta para crear link de pago
+router.post('/mercadopago', (req, res) => {
+    //Creamos objeto de preferencia
+    let preference = {
+        "back_urls": {
+            "success": req.body.back_urls.success,
+            "failure": req.body.back_urls.failure,
+            "pending": req.body.back_urls.pending
+        },
+        items: [req.body.items],
+        marketplace_fee: 0.5
+    };
+
+    mercadopago.preferences.create(preference).then(function (response) {
+        res.send({ data: response.body });
+        console.log(response.body);
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+//ruta para revisar existencia de usuario y correo
+router.post('/validar', async (req, res) => {
+    const {correo} = req.body;
+
+    const user = await User.findOne({correo});
+
+    if(user){
+        res.send({data: 'unavalaible'})
+    }else{
+        res.send({data: 'avalaible'});
+    }
+});
+
 //ruta para registrar un usuario
 router.post('/signup', async (req, res) => {
-    const {correo, nombre, password, nivel, rol, materias} = req.body;
-    const newUser = new User({correo,nombre,password,nivel,rol, materias});
+    const { correo, nombre, password, nivel, rol, materias, credenciales } = req.body;
+
+    //validamos si el correo ya esta registrado
+
+    const newUser = new User({ correo, nombre, password, nivel, rol, materias, credenciales });
     await newUser.save();
 
     //metodo sing de jsonwebtoken que creara el token
-    const token = jwt.sign({_id: newUser._id},'secretKey');
+    const token = jwt.sign({ _id: newUser._id }, 'secretKey');
     console.log(newUser);
 
     //la respuesta ahora sera un token
-    res.status(200).json({token});
+    res.status(200).json({ token });
 });
 
 //ruta para volver a iniciar sesion de usuario
-router.post('/signin', async (req,res)=>{
+router.post('/signin', async (req, res) => {
     const { correo, password } = req.body;
     //find one devuelve un solo objeto y nos permite hacer la comparacion
-    const user = await User.findOne({correo});
+    const user = await User.findOne({ correo });
     //verificamos si el usuario existe en la base de datos
-    if(!user) return res.status(401).send('El correo no existe');
+    if (!user) return res.status(401).json({mensaje: 'El correo no existe'});
     //validamos la contraseña
-    if(user.password !== password) return res.status(401).send('Contraseña erronea');
+    if (user.password !== password) return res.status(401).json({mensaje: 'Contraseña erronea'});
 
     //obtenemos el token
-    const token = jwt.sign({_id: user._id}, 'secretKey');
-    return res.status(200).json({token,user});
+    const token = jwt.sign({ _id: user._id }, 'secretKey');
+    return res.status(200).json({ token, user });
 });
 
 //ruta para nuevo mensaje
-router.post('/newMessage',async(req,res) => {
-    const {chatName, contenido, emisor, receptor} = req.body;
+router.post('/newMessage', async (req, res) => {
+    const { chatName, contenido, emisor, receptor } = req.body;
 
-    const mensaje = new Mensaje({chatName, contenido, emisor, receptor});
+    const mensaje = new Mensaje({ chatName, contenido, emisor, receptor });
     await mensaje.save();
     console.log(mensaje);
     return res.send('Mensaje enviado');
 });
 
 //ruta para obtener mensajes
-router.get('/mensajes',verifyToken,(req,res) => {
+router.get('/mensajes', verifyToken, (req, res) => {
     console.log(req.userId);
-    Mensaje.find({"receptor": ObjectId(req.userId)}).lean().exec((err,doc) => {
+    Mensaje.find({ "receptor": ObjectId(req.userId) }).lean().exec((err, doc) => {
         if (doc.length > 0) {
             //console.log(doc);
-            res.send({data: doc});
-        }else{
+            res.send({ data: doc });
+        } else {
             console.log(err);
-            res.send({ success: false, message: 'No documents retrieved'});
+            res.send({ success: false, message: 'No documents retrieved' });
         }
     });
 });
 
-router.get('/private', verifyToken, (req, res)=>{
-    Tarea.find().lean().exec((err,doc) => {
-        if (doc.length>0) {
+router.get('/private', verifyToken, (req, res) => {
+    Tarea.find().lean().exec((err, doc) => {
+        if (doc.length > 0) {
             console.log(doc);
             res.send({ data: doc });
-        }else{
+        } else {
             console.log(err);
-            res.send({ success: false, message: 'No documents retrieved'});
+            res.send({ success: false, message: 'No documents retrieved' });
         }
     });
 });
 
 
 //ruta para obtener el usuario actual
-router.get('/home',verifyToken,(req,res)=>{    
-    User.find({"_id" : ObjectId(req.userId)}).lean().exec((err,doc) => {
+router.get('/home', verifyToken, (req, res) => {
+    User.find({ "_id": ObjectId(req.userId) }).lean().exec((err, doc) => {
         if (doc.length > 0) {
             //console.log(doc);
-            res.send({data: doc});
-        }else{
+            res.send({ data: doc });
+        } else {
             console.log(err);
-            res.send({ success: false, message: 'No documents retrieved'});
+            res.send({ success: false, message: 'No documents retrieved' });
         }
     });
 });
 
 //ruta para obtener los usuarios que coincidan con los parametros
-router.post('/users',verifyToken,(req,res) => {
-    const {nivel,rol} = req.body;
-    User.find({ nivel: nivel, rol: rol}).lean().exec((err,doc) => {
-        if(doc.length > 0){
+router.post('/users', verifyToken, (req, res) => {
+    const { nivel, rol } = req.body;
+    User.find({ nivel: nivel, rol: rol }).lean().exec((err, doc) => {
+        if (doc.length > 0) {
             //console.log(doc);
-            res.send({data: doc})
-        }else{
+            res.send({ data: doc })
+        } else {
             console.log(err);
             res.send({ success: false, message: 'No documents retrieved' })
         }
@@ -113,24 +188,24 @@ router.post('/users',verifyToken,(req,res) => {
 module.exports = router;
 
 //para verificar la existencia del token en las rutas privadas
-function verifyToken(req, res, next){
+function verifyToken(req, res, next) {
     //revisamos si existe la cabecera authorization
     if (!req.headers.authorization) {
         return res.status(401).send('Unthorize Request');
-    }    
+    }
 
     //separamos el token que nos va a mandar el header
     const token = req.headers.authorization.split(' ')[1];
 
     //si el token obtenido esta vacio
-    if(token === null){
+    if (token === null) {
         return res.status(401).send('Unathorize Request');
     }
 
     //verificamos el token obtenido
-    const payload = jwt.verify(token,'secretKey');
-    
+    const payload = jwt.verify(token, 'secretKey');
+
     //asinamos el id que obtuvimos del token
     req.userId = payload._id;
-    next(); 
+    next();
 }
