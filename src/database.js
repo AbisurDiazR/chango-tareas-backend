28b031bdd8f5af6579f33b8bd7dfdfc21001547c
AbisurDiazR <abisurdiazramirez@gmail.com>
2021-08-15 23:13:19 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://changotareas.ml:27017/changotareas",{
+mongoose.connect("mongodb://koizumi:aiShinozaki23@changotareas.com:27017/?changotareas=admin", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(db=>console.log('Database connected')).catch(err=>console.log(err));
+}).then(db => console.log('Database connected')).catch(err => console.log(err));
